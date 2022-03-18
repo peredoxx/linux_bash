58a74241699cfedc0fd7570dba47b0fda2985320
peredoxx <81801238+peredoxx@users.noreply.github.com>
2022-03-18 15:23:44 +0100

    Update uptime_kuma_install.sh

--- uptime_kuma_install.sh
+++ uptime_kuma_install.sh
@@ -7,8 +7,10 @@ case "$choice" in
   y|Y ) 
 
   #Deploy the Portainer Agent
-
-  docker run -d --restart=always -p 80:3001 -v uptime-kuma:/app/data --name uptime-int louislam/uptime-kuma:1
+  
+  docker volume create uptime-kuma
+  
+  docker run -d --restart=always -p 80:3001 -v uptime-kuma:/app/data --name uptime-int louislam/uptime-kuma:latest
   ;;
   
   n|N ) echo "Installation aborted";; 
