3fe3b7b2b3cec6962c9ac0232e3a7e133acd9aea
peredoxx <81801238+peredoxx@users.noreply.github.com>
2022-03-18 15:00:23 +0100

    Initial commit

---
+++ uptime_kuma_install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo Registered user: $USER
+echo Do you want to install Uptime-Kuma?
+read -p "Continue (y/n)?" choice
+case "$choice" in 
+  y|Y ) 
+
+  #Deploy the Portainer Agent
+
+  docker run -d --restart=always -p 80:3001 -v uptime-kuma:/app/data --name uptime-int louislam/uptime-kuma:1
+  ;;
+  
+  n|N ) echo "Installation aborted";; 
+  * ) echo "invalid";;
+esac
