a42a1400ede7200cd5087ba1a975185b6544144b
peredoxx <81801238+peredoxx@users.noreply.github.com>
2022-03-18 15:25:56 +0100

    Update portainer_agent_install.sh

--- portainer_agent_install.sh
+++ portainer_agent_install.sh
@@ -6,7 +6,7 @@ read -p "Continue (y/n)?" choice
 case "$choice" in 
   y|Y ) 
 
-  #Deploy the Portainer Agent
+  #Deploy the Portainer Agent 
 
   docker run -d -p 9001:9001 --name portainer_agent --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/docker/volumes:/var/lib/docker/volumes portainer/agent:2.9.3
   ;;
