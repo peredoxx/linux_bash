9da92b428002d786eee45619be14ef89a1a9cfe3
peredoxx <81801238+peredoxx@users.noreply.github.com>
2022-03-23 18:29:27 +0100

    Update proxmox_initial_ubuntu.sh

--- proxmox_initial_ubuntu.sh
+++ proxmox_initial_ubuntu.sh
@@ -12,3 +12,27 @@ echo ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDJP0UlWs4SDL/6vXwOoiRifRURatDX3MAfeaY
 # Enable automatic patches 
 sudo apt install unattended-upgrades apt-listchanges -y
 sudo dpkg-reconfigure -plow unattended-upgrades
+
+# Modifying /etc/ssh/sshd_conf
+if [[ "${UID}" -ne 0 ]]; then
+    echo " You need to run this script as root"
+    exit 1
+fi
+
+# To directly modify sshd_config.
+
+sed -i 's/#\?\(Port\s*\).*$/\1 22/' /etc/ssh/sshd_config
+sed -i 's/#\?\(PermitRootLogin\s*\).*$/\1 no/' /etc/ssh/sshd_config
+sed -i 's/#\?\(PubkeyAuthentication\s*\).*$/\1 yes/' /etc/ssh/sshd_config
+sed -i 's/#\?\(PermitEmptyPasswords\s*\).*$/\1 no/' /etc/ssh/sshd_config
+sed -i 's/#\?\(PasswordAuthentication\s*\).*$/\1 no/' /etc/ssh/sshd_config
+
+# Check the exit status of the last command
+
+if [[ "${?}" -ne 0 ]]; then
+   echo "The sshd_config file was not modified successfully"
+   exit 1
+fi
+/etc/init.d/ssh restart
+
+exit 0
